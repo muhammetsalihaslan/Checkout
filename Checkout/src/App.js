f50d87cd879ebd6c320807c0bb33b4a5f91c8165
salih <muhammetsalihaslan@gmail.com>
2022-06-30 17:08:20 +0300

    one

---
+++ Checkout/src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from './component/header/Header';
+
+
+
+const App = () => {
+  return (
+    <div>
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Checkout/src/component/header/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1 className="head1 display-3">CheckOut</h1>
+        </div>
+    )
+}
+
+export default Header;
